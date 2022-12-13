4e45ad80f72b0d98f791f42f0fe36fa7afe9e747
IoannaMi <ioannamicha@hotmail.com>
2022-12-13 19:40:30 +0100

    add endpoints
    * add solution_scenarios endpoint
    * add parameters endpoint
    * add users_periods endpoint
    * alter risk_maps to maps (provide map_type)

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ def bar_chart():
     return jsonify({**base_options, **response})
 
 
+
 @application.route("/api/line_chart", methods=["GET", "POST"])
 def line_chart():
 
@@ -78,8 +79,8 @@ def line_chart():
 
     return jsonify({**base_options, **response})
 
-@application.route("/api/risk_maps", methods=["GET", "POST"])
-def risk_maps():
+@application.route("/api/maps", methods=["GET", "POST"])
+def maps():
     
     """The risk_maps returns a geojson with the risk map.
     Inputs: case, scenario, period_id, solution, users
@@ -92,14 +93,71 @@ def risk_maps():
     solution = inputs["solution"]
     db_schema = inputs["case"]
     users = inputs["users"]
-
-    query = f"select * from {db_schema}.risk_data_geojson({period_id}, '{scenario}','{solution}', '{users}');"
+    map_type = inputs["map_type"]
+    
+    query = f"select * from {db_schema}.all_data_geojson({map_type}, {period_id}, '{scenario}','{solution}', '{users}');" 
     engine, connection = establish_db_connection() 
 
     with connection as con:
         response = con.execute(query).fetchone()[0]
     
-
-
     return jsonify(response)
+
+
+
+@application.route("/api/solutions_scenarios/<case>", methods=["GET", "POST"])
+def solutions_scenarios(case):
+    
+    engine, connection = establish_db_connection() 
+    query_scenarios = f"select * from {case}.list_scenario;"
+    query_solutions =  f"select * from {case}.list_solution;"
+
+    with connection as con: 
+        scenarios_response = con.execute(query_scenarios).fetchall()
+        solutions_response = con.execute(query_solutions).fetchall()
+    scenarios = [scenario[0] for scenario in scenarios_response]
+    solutions= [solution[0] for solution in solutions_response]
+    response = {
+        "solutions": solutions,
+        "scenarios": scenarios
+    }
+    
+    return jsonify(response)
+
+@application.route("/api/users_periods/<case>", methods=["GET", "POST"])
+def users_periods(case):
+    
+    engine, connection = establish_db_connection() 
+    query_users = f"select * from {case}.list_users;"
+    query_periods =  f"select period_name from {case}.period;"
+
+    with connection as con: 
+        users_response = con.execute(query_users).fetchall()
+        periods_response = con.execute(query_periods).fetchall()
+    users = [user[0] for user in users_response]
+    periods= [period[0] for period in periods_response]
+    
+    response = {
+        "users": users,
+        "periods": periods
+    }
+    
+    return jsonify(response)
+    
+
+@application.route("/api/parameters/<case>", methods=["GET", "POST"])
+def scenarios(case):
+
+    engine, connection = establish_db_connection() 
+    query = f"select * from {case}.list_parameter;"
+
+    with connection as con: 
+        query_response = con.execute(query).fetchall()
+    
+    parameters = [parameter[0] for parameter in query_response]
+    response = {
+        "parameters" : parameters
+    }
+    return jsonify(parameters)
+
 
